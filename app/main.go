32aab772724d25c74e31af2cc90073d975691aed
Oti254 <otrated555@gmail.com>
2026-06-03 14:18:42 +0300

    handling invalid commands

--- app/main.go
+++ app/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
+	// Prints the prompt
 	fmt.Print("$ ")
+
+	// Reads the user input, stores it in a string
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		errors.New("Unable to read user input")
+	}
+
+	// Printing error message
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
