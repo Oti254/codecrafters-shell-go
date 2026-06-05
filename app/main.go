b447e788ccfc899274ddb6b17e3d6b6fe303f839
Oti254 <otrated555@gmail.com>
2026-06-05 14:40:30 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,9 +22,23 @@ func main() {
 			os.Exit(1)
 		}
 		command = strings.TrimSpace(command)
+		words := strings.Fields(command)
 
 		if command == "exit" {
 			break
+		}
+
+		// Implementing echo
+		if words[0] == "echo" {
+			fmt.Print(words[1])
+			for i, word := range words {
+				if i == 0 || i == 1 {
+					continue
+				}
+				fmt.Print(" " + word)
+			}
+			fmt.Println()
+			continue
 		}
 		// Printing error message
 		fmt.Println(command + ": command not found")
