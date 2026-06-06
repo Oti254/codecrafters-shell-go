eb314fddd3baf85a80a83747748c27f4c8a10906
Oti254 <otrated555@gmail.com>
2026-06-07 01:59:50 +0300

    locating executable files

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -34,6 +35,10 @@ func main() {
 			break
 		}
 
+		// Getting the contents of the PATH variable
+		pathEnv := os.Getenv("PATH")
+		paths := filepath.SplitList(pathEnv)
+
 		// Implementing echo my way
 		if len(words) > 0 && words[0] == "echo" {
 			fmt.Println(strings.Join(words[1:], " "))
@@ -42,12 +47,49 @@ func main() {
 
 		// Type checking of commands
 		if len(words) > 0 && words[0] == "type" {
+			if len(words) < 2 {
+				fmt.Println("type: missing argument")
+				continue
+			}
+
 			if _, exists := builtIn[words[1]]; exists {
 				fmt.Println(words[1] + " is a shell builtin")
 				continue
 			}
-			fmt.Println(words[1] + ": not found")
+
+			// Checking if a filename exists
+			found := false
+			isExec := false
+
+			for _, dir := range paths {
+				path := filepath.Join(dir, words[1])
+				fi, err := os.Stat(path)
+				if err != nil {
+					continue
+				}
+
+				// Check if file is a directory
+				if fi.IsDir() {
+					continue
+				}
+
+				// Checking if the file is executable
+				mode := fi.Mode()
+				isExec = mode&0111 != 0
+
+				found = true
+
+				// Prints the path if the file found is executable
+				if found && isExec {
+					fmt.Printf("%s is %s\n", words[1], path)
+					break
+				}
+			}
+			if !found {
+				fmt.Println(words[1] + ": not found")
+			}
 			continue
+
 		}
 		// Printing error message
 		fmt.Println(command + ": command not found")
